d13fd52f255bf140702c4d09807078372f707f7f
Guy Luz <guyluz11@gmail.com>
2019-09-13 23:41:10 +0300

    Made the switch from python to c, not including the push button

--- SmartDevice/SmartDeviceDart/bin/shered/manage_physical_components/controlloling_pins.dart
+++ SmartDevice/SmartDeviceDart/bin/shered/manage_physical_components/controlloling_pins.dart
@@ -1,45 +1,31 @@
 import 'dart:io';
 
-//  Function to start python script to interact with pins
+import '../shared_variables.dart';
+
+//  Function to start c script to interact with pins
 
 void PinOn(int pinNumber) async {
-  List<String> pythonCommends = List();
-  pythonCommends.add('-c');
-  pythonCommends.add(
-'''
-try:
-   import RPi.GPIO as GPIO;
-   pin = int(''' + pinNumber.toString() + ''');
-   GPIO.setmode(GPIO.BOARD);
-   GPIO.setup(pin, GPIO.OUT);
-   GPIO.output(pin, GPIO.HIGH);
-   exit();
-   
-except Exception as e:
-   print("This is python exeption: " + str(e));
-''');
-  await Process.run('python', pythonCommends).then((ProcessResult results) {
-    print(results.stdout);
-  });
+  try {
+    await Process.run(SharedVariables.getSnapPath() + '/cScripts/turnOn', [])
+        .then((ProcessResult results) {
+      print(results.stdout);
+    });
+  }
+  catch (error) {
+    print('Path/argument 1 is not specified');
+    print('error: ' + error.toString());
+  }
 }
 
 void pinOff(int pinNumber) async {
-  List<String> pythonCommends = List();
-  pythonCommends.add('-c');
-  pythonCommends.add(
-'''
-try:
-   import RPi.GPIO as GPIO;
-   pin = int(''' + pinNumber.toString() + ''');
-   GPIO.setmode(GPIO.BOARD);
-   GPIO.setup(pin, GPIO.OUT);
-   GPIO.output(pin, GPIO.LOW);
-   exit();
-   
-except Exception as e:
-   print("This is python exeption: " + str(e));
-''');
-  await Process.run('python', pythonCommends).then((ProcessResult results) {
-    print(results.stdout);
-  });
+  try {
+    await Process.run(SharedVariables.getSnapPath() + '/cScripts/turnOff', [])
+        .then((ProcessResult results) {
+      print(results.stdout);
+    });
+  }
+  catch(error){
+    print('Path/argument 1 is not specified');
+    print('error: ' + error.toString());
+  }
 }
