d780012e9b5239a3a40f9219fd4451ac95969b68
Guy Luz <guyluz11@gmail.com>
2019-09-17 01:50:08 +0300

    Add button functionality, c code now take pin number from dart code

--- SmartDevice/cScripts/turnOn.c
+++ SmartDevice/cScripts/turnOn.c
@@ -1,16 +1,15 @@
-
 #include <stdio.h>
 #include <wiringPi.h>
 
 // LED Pin - wiringPi pin 0 is BCM_GPIO 17.
 
-#define LED     16
-
-int main (void)
+int main (int argc, char const *argv[])
 {
+  int ledPin = atoi(argv[1]);
   wiringPiSetup () ;
-  pinMode (LED, OUTPUT) ;
-  digitalWrite (LED, HIGH) ;  // On
+  pinMode (ledPin, OUTPUT) ;
+  digitalWrite (ledPin, HIGH) ;  // On
+
   return 0 ;
 }
 
