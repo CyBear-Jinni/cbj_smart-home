2ab5131a9e0b98817add1618593c4bfe702b2b59
Guy Luz <guyluz11@gmail.com>
2019-09-18 23:35:01 +0300

    Dart-sdk version is now easier to update

--- SmartDevice/bashScripts/dartSdkDownload.sh
+++ SmartDevice/bashScripts/dartSdkDownload.sh
@@ -1,17 +1,18 @@
-echo "start"
+#!/bin/bash
+
 architecture="$(uname -p)"
+
+dartSdkPath="https://storage.googleapis.com/dart-archive/channels/stable/release/2.5.0/sdk/dartsdk-linux-"	#	Sdk url: https://dart.dev/tools/sdk/archive
 
 case $architecture in
 	( "arm" | "armv7l" )
-		wget https://storage.googleapis.com/dart-archive/channels/stable/release/2.5.0/sdk/dartsdk-linux-arm-release.zip  # For arm
+		wget "${dartSdkPath}arm-release.zip"	#	For arm
    	;; 
    	( "arm64" | "aarch64" )
-		wget https://storage.googleapis.com/dart-archive/channels/stable/release/2.5.0/sdk/dartsdk-linux-arm64-release.zip  # For arm64
+		wget "${dartSdkPath}arm64-release.zip"	# For arm64
     ;; 
     ( "x64" | "x86_64" )
-    	wget https://storage.googleapis.com/dart-archive/channels/stable/release/2.5.0/sdk/dartsdk-linux-x64-release.zip    # For linux intel 64
+    	wget "${dartSdkPath}x64-release.zip"	# For linux intel 64
     ;;
 esac
 
-# sdk url: https://dart.dev/tools/sdk/archive
-      
