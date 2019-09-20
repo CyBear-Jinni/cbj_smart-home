195c244483b737f21e30711ef45c00a4d4791ed1
Guy Luz <guyluz11@gmail.com>
2019-09-20 22:57:11 +0300

    Remove unused folder "protoc_as_dart" that out side of the projects

--- ProtoFiles/proto_script.sh
+++ ProtoFiles/proto_script.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 # You mast have pub and protoc inside the path 
-# Exaple PATH=$PATH:~/Programmes/flutter/bin/cache/dart-sdk/bin
+# Example PATH=$PATH:~/Programmes/flutter/bin/cache/dart-sdk/bin
 
 pub global activate protoc_plugin
 
 export PATH="$PATH":"$HOME/.pub-cache/bin"
+
+mkdir protoc_as_dart
 
 protoc --dart_out=grpc:protoc_as_dart/ -Iprotos protos/smart_connection.proto
 
 
 cp -r protoc_as_dart/ ../SmartDevice/SmartDeviceDart/bin/shered/server/	#   # copy to smart device protoc folder
 
-
 cp -r protoc_as_dart/ ../FlutterApp/smart_home_flutter/lib/objects/smart_device/client/	# Copy to Flutter protoc folder 
 
+rm -r protoc_as_dart
