71fceb662571bbd5cb7dc262812427e18aad46c0
Guy Luz <guyluz11@gmail.com>
2019-09-28 23:23:18 +0300

    Listening to button press is now by pin and not onOff variable

--- SmartDevice/SmartDeviceDart/lib/smart_device/smart_device_objects/static_devices/blinds_object.dart
+++ SmartDevice/SmartDeviceDart/lib/smart_device/smart_device_objects/static_devices/blinds_object.dart
@@ -1,8 +1,10 @@
 import '../abstract_smart_devices/smart_device_static_abstract.dart';
 
 class BlindsObject extends SmartDeviceStaticAbstract {
-  BlindsObject(String macAddress, String deviceName, int onOffPinNumber,
-      {onOffButtonPinNumber})
+  BlindsObject(macAddress, deviceName, onOffPinNumber,
+      upButtonPinNumber, int downButtonPinNumber)
       : super(macAddress, deviceName, onOffPinNumber,
-      onOffButtonPinNumber: onOffButtonPinNumber);
+      onOffButtonPinNumber: upButtonPinNumber) {
+
+  }
 }
