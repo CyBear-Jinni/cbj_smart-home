304e0f21288aa6b1ee13292634c362dc9f753a7e
Guy Luz <guyluz11@gmail.com>
2019-09-28 23:23:18 +0300

    Listening to button press is now by pin and not onOff variable

--- SmartDevice/SmartDeviceDart/lib/smart_device/smart_device_objects/dinamic_devices/dynamic_light_object.dart
+++ SmartDevice/SmartDeviceDart/lib/smart_device/smart_device_objects/dinamic_devices/dynamic_light_object.dart
@@ -1,8 +1,7 @@
 import '../abstract_smart_devices/smart_device_dynamic_abstract.dart';
 
 class DynamicLightObject extends SmartDeviceDynamicAbstract {
-  DynamicLightObject(String macAddress, String deviceName, int onOffPinNumber,
-      {onOffButtonPinNumber})
-      : super(macAddress, deviceName, onOffPinNumber,
+  DynamicLightObject(macAddress, deviceName, onOffPinNumber,
+      {onOffButtonPinNumber}) : super(macAddress, deviceName, onOffPinNumber,
       onOffButtonPinNumber: onOffButtonPinNumber);
 }
--- SmartDevice/SmartDeviceDart/lib/smart_device/smart_device_objects/simple_devices/thermostat_object.dart
+++ SmartDevice/SmartDeviceDart/lib/smart_device/smart_device_objects/simple_devices/thermostat_object.dart
@@ -1,8 +1,7 @@
 import '../abstract_smart_devices/smart_device_simple_abstract.dart';
 
 class ThermostatObject extends SmartDeviceSimpleAbstract {
-  ThermostatObject(String macAddress, String deviceName, int onOffPinNumber,
-      {onOffButtonPinNumber})
-      : super(macAddress, deviceName, onOffPinNumber,
+  ThermostatObject(macAddress, deviceName, onOffPinNumber,
+      {onOffButtonPinNumber}) : super(macAddress, deviceName, onOffPinNumber,
       onOffButtonPinNumber: onOffButtonPinNumber);
 }
