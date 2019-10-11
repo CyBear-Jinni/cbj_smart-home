5fd64d869fc5b17d9e7aa0e100add6e21995ccc8
Guy Luz <guyluz11@gmail.com>
2019-10-11 23:15:06 +0300

    Add support for blinds

---
+++ SmartDevice/SmartDeviceDart/lib/shered/wish_classes/blinds_wish.dart
@@ -0,0 +1,53 @@
+import 'package:SmartDeviceDart/smart_device/smart_device_objects/static_devices/blinds_object.dart';
+
+import 'off_wish.dart';
+import 'on_wish.dart';
+
+//  This class take the request to turn up or down the blinds and act in a safe manner with the pins, act so that blinds up and down will not work simultaneously and stop other tasks on them
+class BlindsWish {
+  static String BlindsUp(BlindsObject blindsInformation) {
+    String status;
+    print("Turning blind up");
+
+    blindsInformation.blindsUpPin.onDuration = -1;
+    blindsInformation.blindsDownPin.onDuration = 0;
+
+    status = OffWish.SetOff(
+        blindsInformation.deviceInformation, blindsInformation.blindsDownPin);
+    status += " " +
+        OnWish.SetOn(
+            blindsInformation.deviceInformation, blindsInformation.blindsUpPin);
+
+    return status;
+  }
+
+  static String BlindsDown(BlindsObject blindsInformation) {
+    String status;
+
+    blindsInformation.blindsDownPin.onDuration = -1;
+    blindsInformation.blindsUpPin.onDuration = 0;
+
+    status = OffWish.SetOff(
+        blindsInformation.deviceInformation, blindsInformation.blindsUpPin);
+    status += " " +
+        OnWish.SetOn(blindsInformation.deviceInformation,
+            blindsInformation.blindsDownPin);
+
+    return status;
+  }
+
+  static String BlindsStop(BlindsObject blindsInformation) {
+    String status;
+
+    blindsInformation.blindsDownPin.onDuration = 0;
+    blindsInformation.blindsUpPin.onDuration = 0;
+
+    status = OffWish.SetOff(
+        blindsInformation.deviceInformation, blindsInformation.blindsUpPin);
+    status += " " +
+        OffWish.SetOff(blindsInformation.deviceInformation,
+            blindsInformation.blindsDownPin);
+
+    return status;
+  }
+}
