a13b1f7e0a654dfff2e081d226b99123c054c299
Guy Luz <guyluz11@gmail.com>
2019-11-27 21:07:02 +0200

    The program will show available wif-fi networks when the internet disconnects.

--- NetworkManager/NetworkManagerDart/lib/network_actions.dart
+++ NetworkManager/NetworkManagerDart/lib/network_actions.dart
@@ -1,34 +1,76 @@
 //  Network action class used for controlling the program in the different network status
 
+import 'dart:async';
+import 'dart:io';
+
 import 'package:data_connection_checker/data_connection_checker.dart';
 
-Future<bool> check() async {
-  // Simple check to see if we have internet
-  print("The statement 'this machine is connected to the Internet' is: ");
-  print(await DataConnectionChecker().hasConnection);
-  // returns a bool
+class NetworkActions {
 
-  // We can also get an enum instead of a bool
-  print("Current status: ${await DataConnectionChecker().connectionStatus}");
-  // prints either DataConnectionStatus.connected
-  // or DataConnectionStatus.disconnected
+  //  This function starts the connection to the requested wi-fi if the internet connection is down
+  Future isConnectedToTheInternet() async {
+    print("Status is " + (await isConnectedToInternet()).toString());
+    bool processLocation = false; //  true = Started the process to connect to the admin wi-fi, false = waiting for the internet to go down
 
-  // This returns the last results from the last call
-  // to either hasConnection or connectionStatus
-  print("Last results: ${DataConnectionChecker().lastTryResults}");
+    Stream<DataConnectionStatus> listener = returnStatusIfChanged();
 
-  // actively listen for status updates
-  var listener = DataConnectionChecker().onStatusChange.listen((status) {
+    listener.listen((status) async {
+      bool isConnected = connectionStatusToBool(status);
+      if (isConnected) {
+        print("Connected to the Internet");
+        processLocation = false;
+      }
+      else {
+        print("Does not connected to the Internet");
+        processLocation = true;
+        await Future.delayed(
+            const Duration(seconds: 15)); // Wait to check if internet is back
+
+        // Continue only if internet is till off after timeout
+        if (!processLocation) {
+          return;
+        }
+
+        //  TODO: Get the available networks
+        await getAvailableNetworksList();
+        //  TODO: Keep until the admin wi-fi founded
+        //  TODO: Check in between if connection returned with processLocation var
+        //  TODO: Connect to admin wi-fi
+
+        print("Finally");
+      }
+    });
+  }
+
+  //  This function return the new value of the internet connection status only if it changed from last time
+  Stream<DataConnectionStatus> returnStatusIfChanged() {
+    return DataConnectionChecker().onStatusChange;
+  }
+
+  Future<bool> isConnectedToInternet() async {
+    return connectionStatusToBool(
+        await DataConnectionChecker().connectionStatus);
+  }
+
+  //  Convert connection status to true (connected) false (disconnected)
+  bool connectionStatusToBool(DataConnectionStatus status) {
     switch (status) {
       case DataConnectionStatus.connected:
-        print('Data connection is available.');
-        break;
+        return true;
       case DataConnectionStatus.disconnected:
-        print('You are disconnected from the internet.');
-        break;
+        return false;
     }
-  });
-  // close listener after 30 seconds, so the program doesn't run forever
-  await Future.delayed(Duration(seconds: 30));
-  await listener.cancel();
+    return false;
+  }
+
+  Future<List<String>> getAvailableNetworksList() async {
+    return await Process.run('nmcli',
+        ['dev' + 'wifi']).then((ProcessResult results) {
+      print(results.stdout.toString());
+      print('By by here');
+      List<String> wifi_results = List();
+      wifi_results.add('asd');
+      return wifi_results;
+    });
+  }
 }
