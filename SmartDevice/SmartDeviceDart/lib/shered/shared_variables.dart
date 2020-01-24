5fed2bea98a1d90242888a708606834f29514ff6
Guy Luz <guyluz11@gmail.com>
2020-01-24 13:51:17 +0200

    Snapcraft demo.C file now get the location of the model from outside, add to the snap the dependencies for snowboy

--- SmartDevice/SmartDeviceDart/lib/shered/shared_variables.dart
+++ SmartDevice/SmartDeviceDart/lib/shered/shared_variables.dart
@@ -2,12 +2,12 @@
 
 
 class SharedVariables {
-  static String
-  _snapPath; //  Save the location of all the files that were created during the snapcraft
+  static String _snapPath; //  Save the location of all the files that were created during the snapcraft
 
 
   SharedVariables(String snapPath) {
     _snapPath = snapPath;
+    print("PATH: " + _snapPath);
   }
 
 
