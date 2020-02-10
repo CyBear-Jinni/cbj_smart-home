190dc1a5e2d950909c85374d685d50cd1ad06c24
Guy Luz <guyluz11@gmail.com>
2020-02-10 09:20:32 +0200

    Add splash screen logo, add tests

--- SmartDevice/SmartDeviceDart/lib/shered/server/smart_server.dart
+++ SmartDevice/SmartDeviceDart/lib/shered/server/smart_server.dart
@@ -115,10 +115,10 @@ class SmartServer extends SmartServerServiceBase {
 Future<String> getIps() async {
   for (NetworkInterface interface in await NetworkInterface.list()) {
 //      print('== Interface: ${interface.name} ==');
-    for (InternetAddress addr in interface.addresses) {
+    for (InternetAddress address in interface.addresses) {
 //        print(
 //            '${addr.address} ${addr.host} ${addr.isLoopback} ${addr.rawAddress} ${addr.type.name}');
-      return addr.address;
+      return address.address;
     }
   }
   return null;
---
+++ SmartDevice/SmartDeviceDart/test/device_information_test.dart
@@ -0,0 +1,38 @@
+import 'package:SmartDeviceDart/shered/device_information.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('DeviceInformation', () {
+    String deviceMacAddress = '30:23:a2:G3:34';
+    String deviceName = 'Guy ceiling light';
+
+    DeviceInformation deviceInformation =
+        DeviceInformation(deviceMacAddress, deviceName);
+
+    expect(deviceInformation.GetMacAddress(), deviceMacAddress);
+    expect(deviceInformation.GetName(), deviceName);
+  });
+
+  test('RemoteDevice', () {
+    String deviceMacAddress = '30:23:a2:G3:34';
+    String deviceName = 'Guy ceiling light';
+    String deviceIp = '10.0.0.10';
+
+    RemoteDevice deviceInformation =
+        RemoteDevice(deviceMacAddress, deviceName, deviceIp);
+
+    expect(deviceInformation.GetMacAddress(), deviceMacAddress);
+    expect(deviceInformation.GetName(), deviceName);
+    expect(deviceInformation.GetIp(), deviceIp);
+  });
+
+  test('LocalDevice', () {
+    String deviceMacAddress = "30:23:a2:G3:34";
+    String deviceName = "Guy ceiling light";
+
+    LocalDevice deviceInformation = LocalDevice(deviceMacAddress, deviceName);
+
+    expect(deviceInformation.GetMacAddress(), deviceMacAddress);
+    expect(deviceInformation.GetName(), deviceName);
+  });
+}
