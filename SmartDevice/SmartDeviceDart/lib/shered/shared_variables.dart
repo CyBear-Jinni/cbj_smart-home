d606995585b3b3dafe0a3c3996e74928ab53a304
Guy Luz <guyluz11@gmail.com>
2020-02-22 14:48:49 +0200

    Starting the move to architecture and injection for the flutter app

--- SmartDevice/SmartDeviceDart/lib/shered/shared_variables.dart
+++ SmartDevice/SmartDeviceDart/lib/shered/shared_variables.dart
@@ -2,14 +2,14 @@
 
 
 class SharedVariables {
-  static String _snapPath; //  Save the location of all the files that were created during the snapcraft
+  static String _projectRootDirectoryPath; //  Save the location of all the files that were created during the snapcraft
 
 
-  SharedVariables(String snapPath) {
-    _snapPath = snapPath;
-    print("PATH: " + _snapPath);
+  SharedVariables(String projectRootDirectoryPath) {
+    _projectRootDirectoryPath = projectRootDirectoryPath;
+    print("PATH: " + _projectRootDirectoryPath);
   }
 
 
-  static String getSnapPath() => _snapPath;
+  static String GetProjectRootDirectoryPath() => _projectRootDirectoryPath;
 }
---
+++ SmartDevice/SmartDeviceDart/test/controlloling_pins_test.dart
@@ -0,0 +1,25 @@
+import 'package:SmartDeviceDart/shered/manage_physical_components/controlloling_pins.dart';
+import 'package:SmartDeviceDart/shered/manage_physical_components/devices_pin_configuration/pin_information.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('PinInformation PinInformation.pinAndPhysicalPinConfiguration is null',
+      () async {
+    PinInformation pinInformation = PinInformation();
+
+    String pinOnResponse = await PinOn(pinInformation);
+
+    expect(pinOnResponse,
+        'Error PinInformation.pinAndPhysicalPinConfiguration was not set');
+  });
+
+  test('PinInformation', () async {
+    PinInformation pinInformation =
+        PinInformation(pinAndPhysicalPinConfiguration: 2);
+
+    String pinOnResponse = await PinOn(pinInformation);
+
+    expect(pinOnResponse,
+        'Error SharedVariables.GetProjectRootDirectoryPath was not set');
+  });
+}
