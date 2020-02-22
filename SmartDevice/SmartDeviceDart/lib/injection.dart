76626a492180100ed443e36b38169f6a9afb4ad1
Guy Luz <guyluz11@gmail.com>
2020-02-22 21:48:16 +0200

    Made the

--- SmartDevice/SmartDeviceDart/lib/injection.dart
+++ SmartDevice/SmartDeviceDart/lib/injection.dart
@@ -1,12 +1,13 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:smart_home_flutter/injection.iconfig.dart';
+
+import 'injection.iconfig.dart';
 
 final getIt = GetIt.instance;
 
 @injectableInit
 Future<void> configureInjection(String environment) async =>
-    $initGetIt(getIt, environment: environment);
+    await $initGetIt(getIt, environment: environment);
 
 abstract class Env {
   static const test = 'test';
