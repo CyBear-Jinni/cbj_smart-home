9497a319ce0f20c29c98cd6131625a11098d583f
Guy Luz <guyluz11@gmail.com>
2020-02-22 15:36:22 +0200

    Changing smart device project folder structure to be according to the new tdd architecture and adding the librarys for it.

--- SmartDevice/SmartDeviceDart/test/smart_device_dart_test.dart
+++ SmartDevice/SmartDeviceDart/test/smart_device_dart_test.dart
@@ -1,8 +1,8 @@
-import 'package:SmartDeviceDart/emptyConsoleApplicationDart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
+//import 'package:SmartDeviceDart/emptyConsoleApplicationDart.dart';
+//import 'package:test/test.dart';
+//
+//void main() {
+//  test('calculate', () {
+//    expect(calculate(), 42);
+//  });
+//}
