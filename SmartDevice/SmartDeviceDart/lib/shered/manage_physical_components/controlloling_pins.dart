8cbfe73125c6aabbccb15330e57a7985f6518ef2
Guy Luz <guyluz11@gmail.com>
2020-02-22 14:48:49 +0200

    Starting the move to architecture and injection for the flutter app

--- SmartDevice/SmartDeviceDart/lib/shered/manage_physical_components/controlloling_pins.dart
+++ SmartDevice/SmartDeviceDart/lib/shered/manage_physical_components/controlloling_pins.dart
@@ -4,19 +4,31 @@ import '../shared_variables.dart';
 import 'devices_pin_configuration/pin_information.dart';
 
 //  Function to start c script to interact with pins
-void PinOn(PinInformation pinNumber) async {
+Future<String> PinOn(PinInformation pinNumber) async {
+  if (pinNumber.pinAndPhysicalPinConfiguration == null) {
+    print('Error PinInformation.pinAndPhysicalPinConfiguration was not set');
+    return 'Error PinInformation.pinAndPhysicalPinConfiguration was not set';
+  }
+  if (SharedVariables.GetProjectRootDirectoryPath() == null) {
+    print('Error SharedVariables.GetProjectRootDirectoryPath was not set');
+    return 'Error SharedVariables.GetProjectRootDirectoryPath was not set';
+  }
+
   try {
-    print("This is the pin number on " +
+    print('This is the pin number on ' +
         pinNumber.pinAndPhysicalPinConfiguration.toString());
-    await Process.run(
-        SharedVariables.getSnapPath() + '/scripts/cScripts/turnOn',
+    return await Process.run(
+        SharedVariables.GetProjectRootDirectoryPath() +
+            '/scripts/cScripts/turnOn',
         [pinNumber.pinAndPhysicalPinConfiguration.toString()]).then((
         ProcessResult results) {
       print(results.stdout);
+      return results.stdout;
     });
   } catch (error) {
     print('Path/argument 1 is not specified');
     print('error: ' + error.toString());
+    return 'Path/argument 1 is not specified';
   }
 }
 
@@ -26,7 +38,8 @@ void pinOff(PinInformation pinNumber) async {
     print("This is the pin number off:  " +
         pinNumber.pinAndPhysicalPinConfiguration.toString());
     await Process.run(
-        SharedVariables.getSnapPath() + '/scripts/cScripts/turnOff',
+        SharedVariables.GetProjectRootDirectoryPath() +
+            '/scripts/cScripts/turnOff',
         [pinNumber.pinAndPhysicalPinConfiguration.toString()]).then((
         ProcessResult results) {
       print(results.stdout);
