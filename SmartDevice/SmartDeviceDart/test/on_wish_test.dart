2af45e5ca3ff9d5b07510f1d924f08278e614feb
Guy Luz <guyluz11@gmail.com>
2020-02-22 14:48:49 +0200

    Starting the move to architecture and injection for the flutter app

--- SmartDevice/SmartDeviceDart/test/on_wish_test.dart
+++ SmartDevice/SmartDeviceDart/test/on_wish_test.dart
@@ -1,14 +1,20 @@
-import 'package:SmartDeviceDart/shered/device_information.dart';
-import 'package:SmartDeviceDart/smart_device/smart_device_objects/simple_devices/light_object.dart';
+import 'package:SmartDeviceDart/shered/manage_physical_components/controlloling_pins.dart';
+import 'package:SmartDeviceDart/shered/manage_physical_components/devices_pin_configuration/pin_information.dart';
 import 'package:test/test.dart';
 
 void main() {
   test('calculate', () {
-    LocalDevice('This is the mac Address', 'This is the name of the device');
-    LightObject lightObject =
-        LightObject("30:23:a2:G3:34", "Guy ceiling light", 11);
-    DeviceInformation("30:23:a2:G3:34", "Guy ceiling light");
-//    OnWish.SetOn(deviceInformation, pinNumber);
+//    LocalDevice('This is the mac Address', 'This is the name of the device');
+//    LightObject lightObject =
+//        LightObject("30:23:a2:G3:34", "Guy ceiling light", 11);
+//    DeviceInformation deviceInformation = LocalDevice("30:23:a2:G3:34", "Guy ceiling light");
+//    SharedVariables('/home/guyl/Documents/programing/git/SmartHome');
+//    print(SharedVariables.GetProjectRootDirectoryPath());
+    PinInformation pinInformation = PinInformation();
+
+//    String a = OnWish.SetOn(deviceInformation, pinInformation);
+//    print('This is a: ' + a);
+    PinOn(pinInformation);
     expect(2, 42);
   });
 }
