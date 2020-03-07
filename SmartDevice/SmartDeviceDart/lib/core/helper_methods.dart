df8505c180f11f26f576b4e818f6fc50d4f81a17
Guy Luz <guyluz11@gmail.com>
2020-03-07 15:25:39 +0200

    change variables to var, and "" into ''

--- SmartDevice/SmartDeviceDart/lib/core/helper_methods.dart
+++ SmartDevice/SmartDeviceDart/lib/core/helper_methods.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 //  Get Ip info
 Future<String> getIps() async {
-  for (NetworkInterface interface in await NetworkInterface.list()) {
+  for (var interface in await NetworkInterface.list()) {
 //      print('== Interface: ${interface.name} ==');
-    for (InternetAddress address in interface.addresses) {
+    for (var address in interface.addresses) {
 //        print(
 //            '${addr.address} ${addr.host} ${addr.isLoopback} ${addr.rawAddress} ${addr.type.name}');
       return address.address;
