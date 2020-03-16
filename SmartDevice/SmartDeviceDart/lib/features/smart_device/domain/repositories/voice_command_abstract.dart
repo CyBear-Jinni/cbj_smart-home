2a988ac7acd13c06d014875e37aeaf89a760c76d
Guy Luz <guyluz11@gmail.com>
2020-03-16 21:52:00 +0200

    Microphone to be closer to the new architecture

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/domain/repositories/voice_command_abstract.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/domain/repositories/voice_command_abstract.dart
@@ -1,3 +1,8 @@
+import 'package:SmartDeviceDart/features/smart_device/data/datasources/server/protoc_as_dart/smart_connection.pb.dart';
+import 'package:SmartDeviceDart/features/smart_device/domain/entities/enums.dart';
+import 'package:SmartDeviceDart/features/smart_device/domain/repositories/smart_device_base_abstract.dart';
+
 abstract class VoiceCommandAbstract {
-  Future<bool> listenToActivateKeyWord();
+	CommendStatus executeWishEnum(SmartDeviceBaseAbstract request,
+	                              WishEnum wishEnum);
 }
