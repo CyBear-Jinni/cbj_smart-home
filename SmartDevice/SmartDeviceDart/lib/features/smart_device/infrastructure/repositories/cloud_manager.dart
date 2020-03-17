66f72c3ac8999ea00c0f1e7e32478aa5de9f3a13
Guy Luz <guyluz11@gmail.com>
2020-03-17 23:52:02 +0200

    Change folder name from data to infrastructure

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/repositories/cloud_manager.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/repositories/cloud_manager.dart
@@ -1,6 +1,6 @@
-import 'package:SmartDeviceDart/features/smart_device/data/datasources/fire_store/cloud_fire_store_listen_to_changes.dart';
 import 'package:SmartDeviceDart/features/smart_device/domain/repositories/cloud_manager_abstract.dart';
 import 'package:SmartDeviceDart/features/smart_device/domain/repositories/smart_device_base_abstract.dart';
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/fire_store/cloud_fire_store_listen_to_changes.dart';
 
 class CloudManager extends CloudManagerAbstract {
 
--- SmartDevice/SmartDeviceDart/test/on_wish_test.dart
+++ SmartDevice/SmartDeviceDart/test/on_wish_test.dart
@@ -1,5 +1,5 @@
-import 'package:SmartDeviceDart/features/smart_device/data/datasources/manage_physical_components/devices_pin_configuration/pin_information.dart';
-import 'package:SmartDeviceDart/features/smart_device/data/datasources/manage_physical_components/pins_datasource/sending_signals_to_pins/pin_on_off.dart';
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/manage_physical_components/devices_pin_configuration/pin_information.dart';
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/manage_physical_components/pins_datasource/sending_signals_to_pins/pin_on_off.dart';
 import 'package:SmartDeviceDart/injection.dart';
 import 'package:test/test.dart';
 
