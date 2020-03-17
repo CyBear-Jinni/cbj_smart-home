8852e414ee1cd492045406d9bf0be085ef2d399d
Guy Luz <guyluz11@gmail.com>
2020-03-18 00:11:43 +0200

    Add the application folder

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/repositories/server_repository.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/repositories/server_repository.dart
@@ -1,15 +1,15 @@
+import 'package:SmartDeviceDart/features/smart_device/application/usecases/actions_to_preform.dart';
 import 'package:SmartDeviceDart/features/smart_device/domain/entities/enums.dart';
 import 'package:SmartDeviceDart/features/smart_device/domain/entities/my_singleton.dart';
 import 'package:SmartDeviceDart/features/smart_device/domain/repositories/smart_device_base_abstract.dart';
 import 'package:SmartDeviceDart/features/smart_device/domain/repositories/smart_server_abstract.dart';
-import 'package:SmartDeviceDart/features/smart_device/domain/usecases/actions_to_preform.dart';
 import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/server/protoc_as_dart/smart_connection.pb.dart';
 
 class ServerRepository implements SmartServerAbstract {
   @override
   CommendStatus executeWishEnum(SmartDevice request, WishEnum wishEnum) {
     var smartDevice =
-        MySingleton.getSmartDevicesList()[int.parse(request.name)];
+    MySingleton.getSmartDevicesList()[int.parse(request.name)];
     ActionsToPreform.executeWishEnum(smartDevice, wishEnum);
     return CommendStatus()..success = smartDevice.onOff;
   }
