8978df6d713abb0e61d9003af99f259558c9e9b9
Guy Luz <guyluz11@gmail.com>
2020-03-27 18:32:16 +0300

    Add python code to do speech to text

---
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/microphone/python_speech_to_text.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+@override
+Future<bool> listenToVoiceCommand() async {
+  try {
+    print('Start talking');
+    await Future.delayed(const Duration(seconds: 2)); //
+    var projectPeth =
+        '/home/guyl/Documents/programing/git/SmartHome/SmartDevice/scripts/pythonScripts/voiceAssistence/';
+    var didItWork = await Process.run(
+            projectPeth + 'venv/bin/python3', [projectPeth + 'main.py'])
+        .then((ProcessResult results) {
+      print(results.stdout.toString());
+    });
+    return didItWork;
+  } catch (error) {
+    print('Path/argument 1 is not specified');
+    print('error: ' + error.toString());
+    return false;
+  }
+}
