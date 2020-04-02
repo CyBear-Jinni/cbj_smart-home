1bebc48e78cb0e4226fbab909247690f51489bcd
Guy Luz <guyluz11@gmail.com>
2020-04-02 11:22:32 +0300

    Record less time

--- SmartDevice/scripts/pythonScripts/voiceAssistence/main.py
+++ SmartDevice/scripts/pythonScripts/voiceAssistence/main.py
@@ -12,10 +12,10 @@ try:
 
     sample_rate = 44100  # 44.1kHz sampling rate
     chunk = 4096  # 2^12 samples for buffer
-    record_secs = 3  # seconds to record
-    # dev_index = 5  # device index found by p.get_device_info_by_index(ii)
+    record_secs = 1  # seconds to record
+    # dev_index = 5 # device index found by p.get_device_info_by_index(ii)
     dev_index = None  # device index found by p.get_device_info_by_index(ii)
-    phrase_time_limit = 5
+    phrase_time_limit = 2
     wav_output_filename = 'test1.wav'  # name of .wav file
 
     def show_devices():
@@ -88,15 +88,15 @@ try:
         print("google recognition")
         google_recognition(r, audio)
         print("sphinx recognition")
-        sphinx_recognition(r, audio)
+        # sphinx_recognition(r, audio)
 
     print("Start now")
-    while True:
-        print("Now record")
-        # record()
-        audio_to_text_microphone()
-        print("Now recogniseing")
-        # audio_to_text()
+    # while True:
+    print("Now record")
+    # record()
+    audio_to_text_microphone()
+    print("Now recogniseing")
+    # audio_to_text()
 
 
 except Exception as e:
