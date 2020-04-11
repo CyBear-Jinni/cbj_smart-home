18daf64b563fe9f4a944751c47fcf63a2ee4b237
Guy Luz <guyluz11@gmail.com>
2020-04-11 14:01:17 +0300

    Adding the injection library

--- SmartDevice/SmartDeviceDart/bin/main.dart
+++ SmartDevice/SmartDeviceDart/bin/main.dart
@@ -1,14 +1,13 @@
 import 'package:SmartDeviceDart/core/shared_variables.dart';
 import 'package:SmartDeviceDart/features/smart_device/application/usecases/smart_device_manager.dart';
 import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/manage_physical_components/device_pin_manager.dart';
-import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/microphone/python_speech_to_text.dart';
 import 'package:SmartDeviceDart/injection.dart';
 
 
 void main(List<String> arguments) async {
   print('Smart device is activaited');
-  await Configuration(Env.dev_pi);
 
+  await configureInjection(Env.dev_pc);
   try {
     SharedVariables(arguments[0]);
   } catch (error) {
