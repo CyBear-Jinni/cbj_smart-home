d66275f953fa15a1f5a9bf49d9b46ce2b8b8c17a
Guy Luz <guyluz11@gmail.com>
2020-04-15 17:43:46 +0300

    Change files to much Domain-Driven Design (DDD)

---
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/application/usecases/smart_device_objects_u/abstracts_devices/smart_device_dynamic_abstract.dart
@@ -0,0 +1,52 @@
+import 'package:SmartDeviceDart/features/smart_device/application/usecases/wish_classes_u/dynamic_wish_u.dart';
+import 'package:SmartDeviceDart/features/smart_device/domain/entities/enums.dart';
+
+import 'smart_device_simple_abstract.dart';
+
+//  Abstract class for smart devices class with dynamic property
+abstract class SmartDeviceDynamicAbstract extends SmartDeviceSimpleAbstract {
+  double dynamicValue; //  Save how much power to do for action
+
+
+  SmartDeviceDynamicAbstract(macAddress, deviceName, onOffPinNumber,
+      {onOffButtonPinNumber}) : super(macAddress, deviceName, onOffPinNumber,
+      onOffButtonPinNumber: onOffButtonPinNumber);
+
+
+  @override
+  Future<String> executeWish(String wishString) async {
+    var wish = convertWishStringToWishesObject(wishString);
+    if(wish == null) return 'Your wish does not exist on ';
+    return _WishInDynamicClass(wish);
+  }
+
+  //  Set dynamic value
+  String _SetDynamicValue() {
+    if (deviceInformation == null) {
+      return 'Device information is missing, cant set dynamic value';
+    }
+    return DynamicWishU.setDynamic(deviceInformation);
+  }
+
+  //  Change dynamic value with open connection
+  String _OpenDynamicValue() {
+    if (deviceInformation == null) {
+      return 'Device information is missing, cant create open connection with dynamic ';
+    }
+    DynamicWishU.openDynamic(deviceInformation);
+    onOff = false;
+    return 'Turn on sucsessfuly';
+  }
+
+  //  All the wishes that are legit to execute from the dynamic class
+  String _WishInDynamicClass(WishEnum wish) {
+    switch (wish) {
+      case WishEnum.SDynamic:
+        return _SetDynamicValue();
+      case WishEnum.ODynamic:
+        return _OpenDynamicValue();
+      default:
+        return wishInSimpleClass(wish);
+    }
+  }
+}
