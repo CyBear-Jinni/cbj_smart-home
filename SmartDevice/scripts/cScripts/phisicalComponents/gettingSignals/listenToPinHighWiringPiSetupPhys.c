d24bfb90c9c77b0deea61490200749255a3062c7
Guy Luz <guyluz11@gmail.com>
2020-04-18 18:23:08 +0300

    Updated dart-sdk version in NetworkManager project

---
+++ SmartDevice/scripts/cScripts/phisicalComponents/gettingSignals/listenToPinHighWiringPiSetupPhys.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <wiringPi.h>
+
+int main(int argc, char const *argv[])
+{
+	int buttonPin = atoi(argv[1]);
+
+	if (wiringPiSetupPhys()  == -1)
+		exit (1) ;
+
+	pinMode (buttonPin, INPUT) ;
+
+	pullUpDnControl (buttonPin, PUD_UP) ;
+
+    while (digitalRead (buttonPin) == HIGH ){   // Wait for button press
+    	delay (10) ;
+    }
+
+ 	exit(0);
+}
