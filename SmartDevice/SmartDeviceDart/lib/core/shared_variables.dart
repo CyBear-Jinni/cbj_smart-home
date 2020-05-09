d158fbd6376c5688bd1c1296fd4bf8c35b7c29ba
guy <guyluz11@gmail.com>
2020-05-09 23:39:20 +0300

    Server now know how to use firebase and defrintiate between change from server request directly and change from firebase database, Updated dependencies versions

--- SmartDevice/SmartDeviceDart/lib/core/shared_variables.dart
+++ SmartDevice/SmartDeviceDart/lib/core/shared_variables.dart
@@ -3,13 +3,20 @@
 
 class SharedVariables {
   static String _projectRootDirectoryPath; //  Save the location of all the files that were created during the snapcraft
+  static String _roomName;
 
-
-  SharedVariables(String projectRootDirectoryPath) {
+  SharedVariables(String projectRootDirectoryPath, String roomName) {
     _projectRootDirectoryPath = projectRootDirectoryPath;
+    _roomName = roomName;
     print('PATH: ' + _projectRootDirectoryPath);
   }
 
 
   static String getProjectRootDirectoryPath() => _projectRootDirectoryPath;
+
+  static void setRoomName(String roomName) {
+    _roomName = roomName;
+  }
+
+  static String getRoomName() => _roomName;
 }
