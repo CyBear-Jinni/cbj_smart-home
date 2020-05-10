85c7805e1e6424ade6b1e575dfb5847d7410e44e
guy <guyluz11@gmail.com>
2020-05-10 19:40:47 +0300

    Moved to the new Firestore initialize method for one time aut

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/core_d/fire_store/data_base_controller.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/core_d/fire_store/data_base_controller.dart
@@ -1,32 +1,33 @@
 import 'dart:async';
 
-import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/core_d/fire_store/cloud_fire_store.dart';
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/core_d/fire_store/cloud_fire_store_d.dart';
 import 'package:firedart/firestore/models.dart';
 
 
 
 class DataBaseController {
-  CloudFireStore _cloudFireStore;
+
+  CloudFireStoreD _cloudFireStoreNewD;
 
 
   DataBaseController() {
-    _cloudFireStore = CloudFireStore();
+    _cloudFireStoreNewD = CloudFireStoreD();
   }
 
   //  This method get data of field in the dataPath
   Future<String> getAllFieldsInPath(String dataPath) async {
-    return await _cloudFireStore.getData(dataPath);
+    return await _cloudFireStoreNewD.getData(dataPath);
   }
 
   //  This method get data of field in the dataPath
   Future<String> getFieldInPath(String dataPath, String fieldName) async {
-    return await _cloudFireStore.getFieldInPath(dataPath, fieldName);
+    return await _cloudFireStoreNewD.getFieldInPath(dataPath, fieldName);
   }
 
   //  This method will return data each time data will be changing in the database
   Stream<Document> listenToChangeOfDataInPath(
       String dataPath) async* {
-    yield* _cloudFireStore.listenToChangeOfDataInPath(dataPath);
+    yield* _cloudFireStoreNewD.listenToChangeOfDataInPath(dataPath);
   }
 
   String getValueOfLamp(Document document, String keyName) {
@@ -41,7 +42,7 @@ class DataBaseController {
 
   Future <String> updateDocument(String dataPath, String fieldToUpdate,
       bool valueToUpdate) {
-    return _cloudFireStore.updateDataInBoolField(
+    return _cloudFireStoreNewD.updateDataInBoolField(
         dataPath, fieldToUpdate, valueToUpdate);
   }
 }
