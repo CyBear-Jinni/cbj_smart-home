5e093dc47d8c990220fc4e01169b5bc701d7dadf
guy <guyluz11@gmail.com>
2020-05-19 14:22:37 +0300

    First commit of assistant

---
+++ Assistant/bin/main.dart
@@ -0,0 +1,13 @@
+import 'package:Assistant/speechToText.dart';
+
+void main(List<String> arguments) {
+//  print('Hello world: ${assistant.calculate()}!');
+//
+//  MyAssistant myAssist = MyAssistant();
+//
+//  myAssist.a();
+
+  SpeechToText speechToText = SpeechToText();
+
+  speechToText.start();
+}
