e0cfb9c1c0e34b1f52ca6a095e5aad9cad8bcd8e
guy <guyluz11@gmail.com>
2020-05-19 14:22:37 +0300

    First commit of assistant

---
+++ Assistant/lib/fail_attempts/recognize_response_expend.dart
@@ -0,0 +1,9 @@
+//
+//
+//import 'package:Assistant/speech/google/cloud/speech/v1/cloud_speech.pbgrpc.dart';
+//import 'package:grpc/src/client/channel.dart';
+//
+//class RecognizeResponseExpend extends SpeechClient {
+//  RecognizeResponseExpend(ClientChannel channel) : super(channel);
+//
+//}
