0a411d35633d9845fee876c5a8bed37b9c19fe56
guy <guyluz11@gmail.com>
2020-05-26 00:28:17 +0300

    Activation key world now use Jinni, reverted smart device changes

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/application/usecases/core_u/smart_device_manager_u.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/application/usecases/core_u/smart_device_manager_u.dart
@@ -3,12 +3,13 @@ import 'package:SmartDeviceDart/features/smart_device/application/usecases/cloud
 import 'package:SmartDeviceDart/features/smart_device/application/usecases/set_devices_u/set_devices_u.dart';
 import 'package:SmartDeviceDart/features/smart_device/application/usecases/smart_server_u/smart_server_u.dart';
 import 'package:SmartDeviceDart/features/smart_device/domain/entities/my_singleton.dart';
-
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/repositories/hot_keyword_r/hot_keyword_r.dart';
 
 class SmartDeviceManagerU {
   SetDevicesU _setDevicesUseCase;
   CloudValueChangeU _cloudValueChangeUseCases;
   SmartServerU _smartServerUseCase;
+  HotKeyWordR _hotKeyWordR;
 
   SmartDeviceManagerU() {
     MySingleton();
@@ -18,6 +19,7 @@ class SmartDeviceManagerU {
 
     _cloudValueChangeUseCases = CloudValueChangeU();
     _smartServerUseCase = SmartServerU();
+    _hotKeyWordR = HotKeyWordR();
 
     SmartDeviceMainAsync();
   }
@@ -28,7 +30,8 @@ class SmartDeviceManagerU {
     _cloudValueChangeUseCases
         .listenToDataBase(); //  Listen to changes in the database for this device
 
-//    _hotKeyword.startListeningToVoiceCommandForever(); // Listen to voice command does not work inside snap, alsa not working as root inside the snap
+    _hotKeyWordR
+        .startListeningToVoiceCommandForever(); // Listen to voice command does not work inside snap, alsa not working as root inside the snap
 
     _smartServerUseCase
         .waitForConnection(); //  Start listen for in incoming connections from the local internet (LAN/Wifi)
