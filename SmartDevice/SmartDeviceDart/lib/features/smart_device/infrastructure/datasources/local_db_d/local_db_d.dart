cf9cca37769a065a70db83e226e19718d0e97897
guy <guyluz11@gmail.com>
2020-06-01 01:50:44 +0300

    Starting to add local database named Hive

---
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/local_db_d/local_db_d.dart
@@ -0,0 +1,9 @@
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/hive_d/hive_d.dart';
+
+class LocalDbD {
+  HiveD _hiveD;
+
+  LocalDbD() {
+    _hiveD = HiveD();
+  }
+}
