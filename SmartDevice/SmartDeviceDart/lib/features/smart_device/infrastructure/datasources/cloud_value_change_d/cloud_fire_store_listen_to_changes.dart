7ebd806c52a5d2a0773fca44f4607aa795779b1e
guy <guyluz11@gmail.com>
2020-06-01 01:50:44 +0300

    Starting to add local database named Hive

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/cloud_value_change_d/cloud_fire_store_listen_to_changes.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/cloud_value_change_d/cloud_fire_store_listen_to_changes.dart
@@ -1,24 +1,26 @@
 import 'package:SmartDeviceDart/core/shared_variables.dart';
-import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/core_d/fire_store/data_base_controller.dart';
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/core_d/fire_store/remote_data_base_controller.dart';
 import 'package:firedart/firestore/models.dart';
 
-class CloudFireStoreListenToChangesDatasource {
+class CloudFireStoreListenToChangesD {
 //  String dataPath = '/SmartHomes/GuyHome/Rooms/';
   String dataPath = '/SmartHomes/GuyHome/Rooms/';
   String restOfThePath = '/DevicesInTheRoom/Stairs and Storage';
-  DataBaseController _dataBaseController;
+  RemoteDataBaseController _dataBaseController;
 
-  CloudFireStoreListenToChangesDatasource() {
-    _dataBaseController = DataBaseController();
+  CloudFireStoreListenToChangesD() {
+    _dataBaseController = RemoteDataBaseController();
   }
 
   Stream<Document> listenAndExecute() async* {
-    String fullDataPath = dataPath + SharedVariables.getRoomName() + restOfThePath;
+    String fullDataPath =
+        dataPath + SharedVariables.getRoomName() + restOfThePath;
     yield* _dataBaseController.listenToChangeOfDataInPath(fullDataPath);
   }
 
   Future<String> updateDocument(String fieldToUpdate, bool valueToUpdate) {
-    String fullDataPath = dataPath + SharedVariables.getRoomName() + restOfThePath;
+    String fullDataPath =
+        dataPath + SharedVariables.getRoomName() + restOfThePath;
     return _dataBaseController.updateDocument(
         fullDataPath, fieldToUpdate, valueToUpdate);
   }
---
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/repositories/local_db_r/local_db_r.dart
@@ -0,0 +1,9 @@
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/local_db_d/local_db_d.dart';
+
+class LocalDbR {
+  LocalDbD _localDbD;
+
+  LocalDbR() {
+    _localDbD = LocalDbD();
+  }
+}
