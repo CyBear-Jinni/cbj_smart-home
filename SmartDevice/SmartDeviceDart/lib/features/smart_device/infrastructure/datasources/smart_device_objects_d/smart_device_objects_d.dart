e95d0372768aeb364ace64e90f14492e4985fbfd
guy <guyluz11@gmail.com>
2020-06-02 19:28:54 +0300

    Read from /etc/cbjinni/deviceConfigs.txt any config for the device deafult connections lights blinds

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/smart_device_objects_d/smart_device_objects_d.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/smart_device_objects_d/smart_device_objects_d.dart
@@ -1,7 +1,7 @@
 import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/bash_commends_d/common_bash_commends_d.dart';
 
 class SmartDeviceObjectsD {
-  static Future<String> getUuidAddress() {
-    return CommonBashCommendsD.getUuidAddressOfCurrentDevice();
+  static Future<String> getUuid() {
+    return CommonBashCommendsD.getUuidOfCurrentDevice();
   }
 }
