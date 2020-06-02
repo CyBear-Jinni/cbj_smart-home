3efa3d51ab57104f5103d3897b56a962664752ee
guy <guyluz11@gmail.com>
2020-06-02 19:28:54 +0300

    Read from /etc/cbjinni/deviceConfigs.txt any config for the device deafult connections lights blinds

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/application/usecases/smart_device_objects_u/abstracts_devices/smart_device_simple_abstract.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/application/usecases/smart_device_objects_u/abstracts_devices/smart_device_simple_abstract.dart
@@ -1,21 +1,20 @@
-import 'package:SmartDeviceDart/features/smart_device/domain/entities/core_e/enums.dart';
+import 'package:SmartDeviceDart/features/smart_device/domain/entities/core_e/enums_e.dart';
 
 import 'smart_device_base_abstract.dart';
 
 //  Abstract class for smart devices with on time property
 abstract class SmartDeviceSimpleAbstract extends SmartDeviceBaseAbstract {
   double
-  howMuchTimeTheDeviceDoingAction; //  How much time the smart device was active (Doing action) continuously
+      howMuchTimeTheDeviceDoingAction; //  How much time the smart device was active (Doing action) continuously
 
-
-  SmartDeviceSimpleAbstract(macAddress, smartInstanceName,
-      onOffPinNumber, {onOffButtonPinNumber}) : super(macAddress, smartInstanceName,
-      onOffPinNumber, onOffButtonPinNumber: onOffButtonPinNumber);
-
+  SmartDeviceSimpleAbstract(uuid, smartInstanceName, onOffPinNumber,
+      {onOffButtonPinNumber})
+      : super(uuid, smartInstanceName, onOffPinNumber,
+            onOffButtonPinNumber: onOffButtonPinNumber);
 
   @override
-  Future<String> executeWishString(String wishString,
-      WishSourceEnum wishSourceEnum) async {
+  Future<String> executeWishString(
+      String wishString, WishSourceEnum wishSourceEnum) async {
     var wish = convertWishStringToWishesObject(wishString);
     return executeWish(wish, wishSourceEnum);
   }
--- SmartDevice/SmartDeviceDart/lib/features/smart_device/application/usecases/smart_device_objects_u/static_devices/blinds_object.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/application/usecases/smart_device_objects_u/static_devices/blinds_object.dart
@@ -2,7 +2,7 @@ import 'package:SmartDeviceDart/features/smart_device/application/usecases/butto
 import 'package:SmartDeviceDart/features/smart_device/application/usecases/devices_pin_configuration_u/pin_information.dart';
 import 'package:SmartDeviceDart/features/smart_device/application/usecases/smart_device_objects_u/abstracts_devices/smart_device_static_abstract.dart';
 import 'package:SmartDeviceDart/features/smart_device/application/usecases/wish_classes_u/blinds_wish_u.dart';
-import 'package:SmartDeviceDart/features/smart_device/domain/entities/core_e/enums.dart';
+import 'package:SmartDeviceDart/features/smart_device/domain/entities/core_e/enums_e.dart';
 import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/core_d/manage_physical_components/device_pin_manager.dart';
 
 class BlindsObject extends SmartDeviceStaticAbstract {
@@ -11,16 +11,20 @@ class BlindsObject extends SmartDeviceStaticAbstract {
   PinInformation buttonPinUp, blindsUpPin, buttonPinDown, blindsDownPin;
 
 
-  BlindsObject(macAddress, smartInstanceName, onOffPinNumber,
+  BlindsObject(
+      uuid,
+      smartInstanceName,
+      onOffPinNumber,
       onOffButtonPinNumber,
-      int blindsUpPin, int upButtonPinNumber, int blindsDownPin, int downButtonPinNumber)
-      : super(macAddress, smartInstanceName, onOffPinNumber,
-      onOffButtonPinNumber: onOffButtonPinNumber) {
-    buttonPinUp = DevicePinListManager().getGpioPin(
-        this, upButtonPinNumber);
+      int blindsUpPin,
+      int upButtonPinNumber,
+      int blindsDownPin,
+      int downButtonPinNumber)
+      : super(uuid, smartInstanceName, onOffPinNumber,
+            onOffButtonPinNumber: onOffButtonPinNumber) {
+    buttonPinUp = DevicePinListManager().getGpioPin(this, upButtonPinNumber);
     buttonPinDown =
-        DevicePinListManager().getGpioPin(
-            this, downButtonPinNumber);
+        DevicePinListManager().getGpioPin(this, downButtonPinNumber);
 
     this.blindsUpPin =
         DevicePinListManager().getGpioPin(this, blindsUpPin);
