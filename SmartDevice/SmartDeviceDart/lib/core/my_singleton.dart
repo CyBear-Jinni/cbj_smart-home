fa6f32c48ca885f56019fd7c0470f00b24412b81
guy <guyluz11@gmail.com>
2020-06-15 12:12:04 +0300

    Loads the smart device from the local db into the singlton of devices

--- SmartDevice/SmartDeviceDart/lib/core/my_singleton.dart
+++ SmartDevice/SmartDeviceDart/lib/core/my_singleton.dart
@@ -4,8 +4,8 @@ import 'package:SmartDeviceDart/features/smart_device/infrastructure/repositorie
 class MySingleton {
   static final MySingleton _singleton = MySingleton._internal();
   static List<SmartDeviceBaseAbstract> smartDevicesList;
-  static String _deviceUid;
-  static String _currentUserName;
+  static Future<String> _deviceUid;
+  static Future<String> _currentUserName;
 
   factory MySingleton() {
     return _singleton;
@@ -17,15 +17,10 @@ class MySingleton {
     getCurrentUserName();
   }
 
-  static Future<String> getUuid() async {
-    _deviceUid ??= await MySingletonHelper.getUuid();
-    return _deviceUid;
-  }
+  static Future<String> getUuid() => _deviceUid ??= MySingletonHelper.getUuid();
 
-  static Future<String> getCurrentUserName() async {
-    _currentUserName ??= await MySingletonHelper.getCurrentUserName();
-    return _currentUserName;
-  }
+  static Future<String> getCurrentUserName() =>
+      _currentUserName ??= MySingletonHelper.getCurrentUserName();
 
   static void setSmartDevicesList(
       List<SmartDeviceBaseAbstract> smartDeviceListVal) {
