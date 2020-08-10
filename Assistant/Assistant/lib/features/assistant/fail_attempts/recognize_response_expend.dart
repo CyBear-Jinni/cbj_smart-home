0d647f20a5dbb143e467e88a84254062f2a073fc
guy <guyluz11@gmail.com>
2020-08-11 00:18:25 +0300

    Removed some of the Firebase info

--- Assistant/Assistant/lib/features/assistant/fail_attempts/recognize_response_expend.dart
+++
@@ -1,9 +0,0 @@
-//
-//
-//import 'package:Assistant/speech/google/cloud/speech/v1/cloud_speech.pbgrpc.dart';
-//import 'package:grpc/src/client/channel.dart';
-//
-//class RecognizeResponseExpend extends SpeechClient {
-//  RecognizeResponseExpend(ClientChannel channel) : super(channel);
-//
-//}
