b2e599a4bec0455d0cc20926b2c10a63d474d248
guy <guyluz11@gmail.com>
2020-08-22 02:05:10 +0300

    Created constant_credentials.dart and add instractions to smartDevice README.md

---
+++ SmartDevice/SmartDeviceDart/lib/core/constant_credentials.dart
@@ -0,0 +1,5 @@
+// Contain the credentials that does not change
+class ConstantCredentials {
+  static const fireBaseProjectId = 'FireBase-Project-Id';
+  static const fireBaseWebApiKey = 'FireBase-Web-Api-Key';
+}
