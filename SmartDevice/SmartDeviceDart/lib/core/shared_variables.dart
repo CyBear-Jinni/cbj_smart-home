82e1d8245eee406e8a58ba3c7d7e9ee23662ee7d
guy <guyluz11@gmail.com>
2020-08-26 11:24:36 +0300

    Removed un used variable

--- SmartDevice/SmartDeviceDart/lib/core/shared_variables.dart
+++ SmartDevice/SmartDeviceDart/lib/core/shared_variables.dart
@@ -2,21 +2,13 @@
 
 
 class SharedVariables {
-  static String _projectRootDirectoryPath; //  Save the location of all the files that were created during the snapcraft
-  static String _roomName;
+  static String
+      _projectRootDirectoryPath; //  Save the location of all the files that were created during the snapcraft
 
-  SharedVariables(String projectRootDirectoryPath, String roomName) {
+  SharedVariables(String projectRootDirectoryPath) {
     _projectRootDirectoryPath = projectRootDirectoryPath;
-    _roomName = roomName;
     print('PATH: ' + _projectRootDirectoryPath);
   }
 
-
   static String getProjectRootDirectoryPath() => _projectRootDirectoryPath;
-
-  static void setRoomName(String roomName) {
-    _roomName = roomName;
-  }
-
-  static String getRoomName() => _roomName;
 }
