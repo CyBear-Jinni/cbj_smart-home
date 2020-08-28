b96e837925b9ba2d308ef52947f7eb9ec3b3ea5e
guy <guyluz11@gmail.com>
2020-08-28 17:29:00 +0300

    First commit of inserting firebase information from the flutter app

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/domain/entities/cloud_value_change_e/cloud_value_change_e.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/domain/entities/cloud_value_change_e/cloud_value_change_e.dart
@@ -1,12 +1,18 @@
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/accounts_information_d/accounts_information_d.dart';
 import 'package:SmartDeviceDart/features/smart_device/infrastructure/repositories/cloud_value_change_r/cloud_value_change_r.dart';
 import 'package:firedart/firestore/models.dart';
 
 class CloudValueChangeE {
   static CloudManagerR _cloudManager;
 
-  CloudValueChangeE() {
-    _cloudManager ??= CloudManagerR();
+  CloudValueChangeE(FirebaseAccountsInformationD firebaseAccountsInformationD) {
+    if (firebaseAccountsInformationD.fireBaseProjectId != null &&
+        firebaseAccountsInformationD.fireBaseApiKey != null &&
+        firebaseAccountsInformationD.userEmail != null &&
+        firebaseAccountsInformationD.userPassword != null) {
+      _cloudManager ??= CloudManagerR(firebaseAccountsInformationD);
 //    MySingleton.getSmartDevicesList()[0];
+    }
   }
 
   Stream<Document> listenToDataBase() async* {
