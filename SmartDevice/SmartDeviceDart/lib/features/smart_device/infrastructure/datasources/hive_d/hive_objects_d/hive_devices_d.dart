f132a8d9953873d375875cb4553efd54bdbd1db8
guy <guyluz11@gmail.com>
2020-08-28 17:29:00 +0300

    First commit of inserting firebase information from the flutter app

--- SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/hive_d/hive_objects_d/hive_devices_d.dart
+++ SmartDevice/SmartDeviceDart/lib/features/smart_device/infrastructure/datasources/hive_d/hive_objects_d/hive_devices_d.dart
@@ -1,3 +1,4 @@
+import 'package:SmartDeviceDart/features/smart_device/infrastructure/datasources/accounts_information_d/accounts_information_d.dart';
 import 'package:hive/hive.dart';
 
 part 'hive_devices_d.g.dart';
@@ -6,4 +7,6 @@ part 'hive_devices_d.g.dart';
 class HiveDevicesD {
   @HiveField(0)
   Map<String, List<String>> smartDeviceList;
+  @HiveField(1)
+  FirebaseAccountsInformationD databaseInformationList;
 }
