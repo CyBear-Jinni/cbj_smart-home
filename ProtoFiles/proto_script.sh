b51b0d355da1f646d81ff25948316920cef23bca
Guy Luz <guyluz11@gmail.com>
2020-12-08 03:21:01 +0200

    App get the Smart Device IP in the hotspot

--- ProtoFiles/proto_script.sh
+++ ProtoFiles/proto_script.sh
@@ -9,7 +9,7 @@ export PATH="$PATH":"$HOME/.pub-cache/bin"
 
 
 
-# Connection between Smart Device and the App
+# Connection between App to Smart Device
 
 mkdir protoc_as_dart
 
@@ -26,7 +26,7 @@ rm -r protoc_as_dart
 
 
 
-# Connection between Security Bear and the App
+# Connection between App to Security Bear
 
 mkdir protoc_as_dart
 
@@ -37,6 +37,26 @@ rm -r ../Security-Bear/SecurityBearDart/lib/features/security_bear/infrastructur
 cp -r protoc_as_dart/ ../Security-Bear/SecurityBearDart/lib/features/security_bear/infrastructure/datasources/security_bear_server_d # Copy to Security Bear protoc folder
 
 rm -r ../CBJ_App/lib/objects/security_bear/client/protoc_as_dart
-cp -r protoc_as_dart/ ../CBJ_App/lib/objects/security_bear/client	# Copy to Flutter protoc folder
+cp -r protoc_as_dart/ ../CBJ_App/lib/objects/security_bear/client	# Copy to CBJ App protoc folder
+
+rm -r protoc_as_dart
+
+
+
+# Connection between Security Bear to the App
+
+mkdir protoc_as_dart
+
+protoc --dart_out=grpc:protoc_as_dart/ -Iprotos protos/cbj_app_connections.proto
+
+
+
+rm -r ../CBJ_App/lib/database/cbj_app_server/protoc_as_dart
+cp -r protoc_as_dart/ ../CBJ_App/lib/database/cbj_app_server	# Copy to CBJ App protoc folder
+
+
+rm -r ../Security-Bear/SecurityBearDart/lib/data_base/cbj_app/protoc_as_dart
+cp -r protoc_as_dart/ ../Security-Bear/SecurityBearDart/lib/data_base/cbj_app # Copy to Security Bear protoc folder
+
 
 rm -r protoc_as_dart
