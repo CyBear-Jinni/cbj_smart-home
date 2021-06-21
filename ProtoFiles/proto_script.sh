e535e91e11a53a2d94fb2cc6c43a5cb8b8221086
Guy Luz <guyluz11@gmail.com>
2021-06-21 15:05:16 +0300

    Adding grpc files and small test for the remote pipes

--- ProtoFiles/proto_script.sh
+++ ProtoFiles/proto_script.sh
@@ -22,6 +22,8 @@ cp -r -p protoc_as_dart/ ../CBJ_Smart-Device/SmartDeviceDart/lib/features/smart_
 rm -r ../CBJ_App/lib/infrastructure/core/gen/smart_device/client/protoc_as_dart
 cp -r protoc_as_dart/ ../CBJ_App/lib/infrastructure/core/gen/smart_device/client/	# Copy to Flutter protoc folder
 
+cp -r -p protoc_as_dart/ ../CBJ_Remote-Pipes/cbj_remote-pipes/lib/infrastructure/smart_device_server_and_client/	#   # copy to Remote-Pipes protoc folder
+
 rm -r protoc_as_dart
 
 
